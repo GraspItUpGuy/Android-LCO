acd3e758840ae16ad924143d9398c1081734729f
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-15 01:21:50 +0530

    button assignment
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

--- AndroidPExerciseFiles/02ButtonProject/app/src/main/java/com/example/mybuttonapplication/MainActivity.java
+++ AndroidPExerciseFiles/02ButtonProject/app/src/main/java/com/example/mybuttonapplication/MainActivity.java
@@ -1,14 +1,46 @@
-package com.example.mybuttonapplication;
+package com.example.mybuttonapplication;// package declaration
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button; // declareing the button
+    Button Btn1;
+    Button Btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // we write our code here
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.mybtn); // found the button by ID
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("test", "Clicked");
+            }
+        });  // event listener on button
+        Btn1 = findViewById(R.id.myBtn1);
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("test1", "Blue Button clicked");
+            }
+        });
+
+        Btn2 = findViewById(R.id.myBtn2);
+        Btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("test 3", "Green Button clicked");
+            }
+        });
     }
 }
